66feb48853c9ae81bc033679cc37077bfc51bb0f
rbgarcia <rbgarcia@gmail.com>
2026-01-04 17:41:20 -0300

    Updated /src/bookfinder_api/core/data/repository.py

--- src/bookfinder_api/core/data/repository.py
+++ src/bookfinder_api/core/data/repository.py
@@ -1,7 +1,7 @@
 import csv
 from typing import List, Dict
 
-from ...schemas.BookOut import BookOut
+from bookfinder_api.core.models.Book import Book
 from ..ingestion.scraper import CSV_PATH
 
 
@@ -9,14 +9,13 @@ def load_books() -> List[Dict]:
     if not CSV_PATH.exists():
         raise FileNotFoundError(f"CSV file not found at {CSV_PATH}")
 
-    books: List[BookOut] = []
+    books: List[Book] = []
 
     with CSV_PATH.open("r", encoding="utf-8") as f:
         reader = csv.DictReader(f)
         for idx, row in enumerate(reader):
             books.append(
-                BookOut(
-                    id=idx,
+                Book(
                     title=row["title"],
                     price=float(row["price"]),
                     rating=int(row["rating"]),
